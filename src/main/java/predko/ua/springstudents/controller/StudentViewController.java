a73a44d41b3ec1f52eb326fb396e912d17c39c35
LevPredko <levpredko@gmail.com>
2024-08-09 23:54:41 +0300

    Implemented: the main page, student display page, add students page, the student data change page, student removal page( #9 #10 #11 #12 #13 )

--- src/main/java/predko/ua/springstudents/controller/StudentViewController.java
+++ src/main/java/predko/ua/springstudents/controller/StudentViewController.java
@@ -4,6 +4,8 @@ import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import predko.ua.springstudents.model.Student;
 import predko.ua.springstudents.service.StudentService;
 
@@ -26,5 +28,38 @@ public class StudentViewController {
     public String addStudentForm(Model model) {
         model.addAttribute("student", new Student());
         return "add-student";
+    }
+
+    @PostMapping ("api/students/add")
+    public String addStudent(Student student) {
+        studentService.saveStudent(student);
+        return "redirect:/students";
+    }
+
+    @GetMapping("/students/update/{id}")
+    public String updateStudentForm(@PathVariable Long id, Model model) {
+        Student student = studentService.findById(id);
+        model.addAttribute("student", student);
+        return "update-student";
+    }
+    @PostMapping ("api/students/update")
+    public String updateStudent(Student student) {
+        studentService.updateStudent(student);
+        return "redirect:/students";
+    }
+
+    @GetMapping("/students/delete/{id}")
+    public String deleteStudentForm(@PathVariable Long id, Model model) {
+        Student student = studentService.findById(id);
+        if (student != null) {
+            model.addAttribute("student", student);
+        }
+        return "delete-student";
+    }
+
+    @PostMapping("/api/students/delete_student/id/{id}")
+    public String deleteStudent(@PathVariable Long id) {
+        studentService.deleteStudentById(id);
+        return "redirect:/students";
     }
 }
